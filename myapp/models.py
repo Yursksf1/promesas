b9a74c911a503f51acc5903d70bd277a08b3e244
yurley <yurs.ksf1@gmail.com>
2022-09-13 20:24:24 -0500

    clean code

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User, AbstractUser
 
 
 class Acudiente(models.Model):
@@ -27,22 +27,18 @@ class Equipo(models.Model):
     def __str__(self):
         return "{} - {}".format(self.name, self.entrenador)
 
-class Jugador(AbstractUser):
-    acudiente = models.ForeignKey(Acudiente, on_delete=models.CASCADE)
-    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE, default=None, null=True, blank=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    fecha_nacimiento = models.DateField()
+class Jugador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None, null=True, blank=True)
     genero = models.CharField(max_length=30, null=True, blank=True, default=None)
     altura = models.IntegerField()
-    
-    class Meta: 
+
+    class Meta:
         verbose_name = "Jugador"
         verbose_name_plural = "Jugadores"
-    
+
 
     def __str__(self):
-        return "{} {} ".format(self.first_name, self.last_name)
+        return "{} {} ".format(self.user.first_name, self.user.last_name)
 
 
 class Subscripcion(models.Model):
@@ -50,7 +46,7 @@ class Subscripcion(models.Model):
     jugador = models.ForeignKey(Jugador, on_delete=models.CASCADE)
     fecha = models.DateField()
 
-    class Meta: 
+    class Meta:
         verbose_name = "Subscription"
         verbose_name_plural = "Subscripciones"
 
@@ -79,12 +75,12 @@ class Entrenamientos_jugador(models.Model):
     class Meta:
         verbose_name = "Entrenamientos_jugador"
         verbose_name_plural = "Entrenamientos_jugadores"
-    
+
 
     def __str__(self):
         return "{} - {} - {} - {} ".format(self.Cantidad_partidos, self.categoria, self.edad, self.jugador,)
 
-    
+
 class Resultado(models.Model):
     jugador = models.ForeignKey(Jugador, on_delete=models.CASCADE)
     partido = models.ForeignKey(Horario_partido, on_delete=models.CASCADE)
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
+from .models import Jugador
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            genero = form.cleaned_data.get('genero')
+            altura = form.cleaned_data.get('altura')
+            jugador = Jugador()
+            jugador.genero = genero
+            jugador.altura = altura
+            jugador.user = user
+            jugador.save()
+
+            user.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignupForm()
+
+    context = {'form': form}
+    return render(request, 'users/signup.html', context)
